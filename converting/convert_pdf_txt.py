0b125a85e0aca86eeea6303af30a0a61f39575cc
gaBUBUNts <89221310+gaBUBUNts@users.noreply.github.com>
2022-10-24 13:27:56 +0300

    upd pdf_to_txt

--- converting/convert_pdf_txt.py
+++ converting/convert_pdf_txt.py
@@ -1,29 +1,21 @@
+from os import mkdir
+from os.path import isdir, join
 from read_report import read_pdf
 
 
-def pdf_to_txt(path: str, idx_beg: int, idx_end: int) -> None:
+def pdf_to_txt(path: str, idx_beg: int, idx_end: int, chapter_id: int, txt_dir_name: str) -> None:
     text = read_pdf(path, idx_beg=idx_beg, idx_end=idx_end)
-    with open(f'{path[:-3]}txt', "w", encoding="utf-8") as f:
-        f.write(text)
+    path_to_txt = path[:-4].replace('pdfs', 'txt/'+txt_dir_name)
+    path_to_txt_dir = join(*path_to_txt.split('/')[:-1])
+
+    if not isdir(path_to_txt_dir):
+        mkdir(path_to_txt_dir)
+
+    with open(path_to_txt + f"_{chapter_id}.txt", "w", encoding="utf-8") as f:
+            f.write(text)
 
 
 def read_txt(path) -> str:
-    with open(f'{path[:-3]}txt', "r", encoding="utf-8") as f:
+    with open(path, "r", encoding="utf-8") as f:
         text = f.read()
     return text
-
-
-if __name__ == '__main__':
-    path = '../reports/pdfs/Архангельское_месторождение_Пересчет_запасов_КГ.pdf'
-    text = read_pdf(path, idx_beg=1, idx_end=293)
-    with open(f'{path[:-3]}txt', "w", encoding="utf-8") as f:
-        f.write(text)
-
-    with open(f'{path[:-3]}txt', "r", encoding="utf-8") as f:
-        text = f.read()
-    print(text)
-
-    # path = '../reports/pdfs/Ивинское, книга 1.pdf'
-    # text = read_pdf(path, idx_beg=1, idx_end=453)
-    # with open(f'{path[:-3]}txt', "w", encoding="utf-8") as f:
-    #     f.write(text)
--- main.py
+++ main.py
@@ -13,6 +13,6 @@ you can do it once and save at txt-fles
     - oil_deposit
 7. XML to xlsx
 """
-with open("../reports/txt/Архангельское_месторождение_Пересчет_запасов_КГ.txt", "r", encoding="utf-8") as f:
-    text = f.read()
-print(text)
+# with open("../reports/txt/Архангельское_месторождение_Пересчет_запасов_КГ.txt", "r", encoding="utf-8") as f:
+#     text = f.read()
+# print(text)
