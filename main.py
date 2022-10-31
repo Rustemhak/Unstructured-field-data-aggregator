8548f14f0965104af16378a84008d0e0c43d31f0
Rustemhak <rustemhak@mail.ru>
2022-10-31 18:13:49 +0300

    add pipeline for kern

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ you can do it once and save at txt-fles
     - oil_deposit
 7. XML to xlsx
 """
+
+"""
+pipeline for table kern
+1. get pd.dataframe (use recognize_to_read_table in kern_table), ar 
+2. take values (objects) from first column "Продуктивный горизонт, ярус"
+3.  in xlsx table for these objects tick "Да", for other "Нет" 
+"""
 # with open("../reports/txt/Архангельское_месторождение_Пересчет_запасов_КГ.txt", "r", encoding="utf-8") as f:
 #     text = f.read()
 # print(text)
