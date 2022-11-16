7bb651b2d08484ae609b7c4fb575c1aacd8a9e84
Rustemhak <rustemhak@mail.ru>
2022-11-16 17:39:54 +0300

    add uplooad pdf and download

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,28 @@
 import streamlit as st
 import pandas as pd
 from download_streamlit import convert_df
+from io import StringIO
+import pdfplumber
 
-d = {'col1': [1, 2], 'col2': [3, 4]}
-my_large_df = pd.DataFrame(data=d)
-csv = convert_df(my_large_df)
+from read_report.read_report import read_report
 
-st.download_button(
-    label="Download data as CSV",
-    data=csv,
-    file_name='large_df.csv',
-    mime='text/csv',
-)
+
+def extract_data(feed):
+    data = []
+    with pdfplumber.load(feed) as pdf:
+        if len(pdf.pages):
+            text = ' '.join([
+                page.dedupe_chars().extract_text(y_tolerance=6) or '' for page in pdf.pages if page
+            ])
+            #pdf to xlsx
+
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    xlsx = extract_data(uploaded_file)
+    st.download_button(
+        label="Download data as xlsx",
+        data=xlsx,
+        file_name='report.xlsx',
+        mime='text/xlsx',
+    )
